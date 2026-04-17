ba5c078ed59279fd505d47eae4a715b569cd153a
Hubert-2 <hubert.biernat@student.wsb.edu.pl>
2026-04-17 12:14:36 +0200

    Update main.py

--- zadania1/kalkulator/python/main.py
+++ zadania1/kalkulator/python/main.py
@@ -4,20 +4,20 @@ operacja = input("Jaką operację chcesz wykonać? (+ - / *): ")
 
 if operacja == "+":
     wynik = liczba1 + liczba2
-    print("Wynik:", wynik)
+    print("Wynik dodawania:", wynik)
 
 elif operacja == "-":
     wynik = liczba1 - liczba2
-    print("Wynik:", wynik)
+    print("Wynik odejmowania:", wynik)
 
 elif operacja == "*":
     wynik = liczba1 * liczba2
-    print("Wynik:", wynik)
+    print("Wynik mnożenia:", wynik)
 
 elif operacja == "/" :
     if liczba2 != 0:
         wynik = liczba1 / liczba2
-        print("Wynik:", wynik)
+        print("Wynik dzielenia:", wynik)
     else:
         print("Nie można dzielić przez zero!")
     
